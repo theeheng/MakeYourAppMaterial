b1440cd514dee3f10483ef073c1974e5022d842a
theeheng <theeheng@yahoo.com>
2015-11-23 12:37:20 +0000

    fix: Image Loading Issue due to https
    Update okhttps and change fetchPlainText function

--- xyzreader/XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
+++ xyzreader/XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
@@ -3,6 +3,8 @@ package com.example.xyzreader.remote;
 import android.util.Log;
 
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,10 +47,13 @@ public class RemoteEndpointUtil {
     }
 
     static String fetchPlainText(URL url) throws IOException {
-        return new String(fetch(url), "UTF-8" );
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 
-    static byte[] fetch(URL url) throws IOException {
+    /*static byte[] fetch(URL url) throws IOException {
         InputStream in = null;
 
         try {
@@ -68,5 +73,5 @@ public class RemoteEndpointUtil {
                 in.close();
             }
         }
-    }
+    }*/
 }
