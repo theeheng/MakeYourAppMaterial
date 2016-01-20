8300b7578508f102b85a8b5922355ca900c0aa7f
theeheng <theeheng@yahoo.com>
2016-01-20 14:59:34 +0000

    feat: add transition to detail activity view control

--- xyzreader/XYZReader/build/generated/source/r/debug/android/support/v7/cardview/R.java
+++ xyzreader/XYZReader/build/generated/source/r/debug/android/support/v7/cardview/R.java
@@ -21,20 +21,20 @@ public final class R {
 		public static final int contentPaddingTop = 0x7f010030;
 	}
 	public static final class color {
-		public static final int cardview_dark_background = 0x7f070012;
-		public static final int cardview_light_background = 0x7f070013;
-		public static final int cardview_shadow_end_color = 0x7f070014;
-		public static final int cardview_shadow_start_color = 0x7f070015;
+		public static final int cardview_dark_background = 0x7f080012;
+		public static final int cardview_light_background = 0x7f080013;
+		public static final int cardview_shadow_end_color = 0x7f080014;
+		public static final int cardview_shadow_start_color = 0x7f080015;
 	}
 	public static final class dimen {
-		public static final int cardview_compat_inset_shadow = 0x7f08003d;
-		public static final int cardview_default_elevation = 0x7f08003e;
-		public static final int cardview_default_radius = 0x7f08003f;
+		public static final int cardview_compat_inset_shadow = 0x7f09003d;
+		public static final int cardview_default_elevation = 0x7f09003e;
+		public static final int cardview_default_radius = 0x7f09003f;
 	}
 	public static final class style {
-		public static final int CardView = 0x7f0d0081;
-		public static final int CardView_Dark = 0x7f0d0082;
-		public static final int CardView_Light = 0x7f0d0083;
+		public static final int CardView = 0x7f0e0081;
+		public static final int CardView_Dark = 0x7f0e0082;
+		public static final int CardView_Light = 0x7f0e0083;
 	}
 	public static final class styleable {
 		public static final int[] CardView = { 0x7f010027, 0x7f010028, 0x7f010029, 0x7f01002a, 0x7f01002b, 0x7f01002c, 0x7f01002d, 0x7f01002e, 0x7f01002f, 0x7f010030, 0x7f010031 };
